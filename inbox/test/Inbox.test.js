08aabba30996ba039dfab60b97e19f4c34380392
Caroline da Silva Nunes <carolinesnunesadm@gmail.com>
2021-10-04 19:08:07 -0300

    mocha test

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,1 +1,50 @@
-// contract test code will go here
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+
+
+//explaining before it, describe and it of Mocha.
+/*
+class Car {
+  park() {
+    return "stopped";
+  }
+  drive() {
+    return "vroom";
+  }
+}
+
+let car;
+
+beforeEach(() => {
+  car = new Car();
+});
+
+describe("Car", () => {
+  it("can park", () => { 
+    assert.equal(car.park(), "stopped");
+  });
+  it("can drive", () => {
+    assert.equal(car.drive(), "vroom");
+  });
+});
+
+*/
+
+//0xa941805
+
+beforeEach(() => {
+    //get a list of all accounts
+    web3.eth.getAccounts()
+        .then(fetchedAccounts => {
+            console.log(fetchedAccounts);
+        })
+
+
+    //use one of those accounts to deploy the contract
+});
+
+describe("Inbox", () => {
+    it("deploys a contract", () => {})
+});
